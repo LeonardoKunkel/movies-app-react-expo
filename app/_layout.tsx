775b9658d49feff8887601821c846257bdc81838
Leonardo <mr_kunkel@hotmail.com>
2026-06-03 14:37:25 -0600

    actions & responses generated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Text, View } from 'react-native'
+import { nowPlayingAction } from '@/core/actions/movies/now-playing.action';
+import { Text, View } from 'react-native';
 
 const RootLayout = () => {
+
+  nowPlayingAction();
+
   return (
     <View>
       <Text className='text3xl'>RootLayout</Text>
